367c7e6b0c41ac53af68eb7c5d638ec260cc7e14
Maciej Kaminski <maciej.kam03@gmail.com>
2025-05-25 13:48:22 +0200

    Added 404 page

---
+++ client/src/components/NotFound.jsx
@@ -0,0 +1,12 @@
+import '../styles/notFound.css'
+
+const NotFound = () => {
+  return (
+    <div className='not-found'>
+      <h1 className='not-found-title'>Page Not Found</h1>
+      <p className='not-found-info'>Sorry, the page you are looking for does not exist.</p>
+      <a href="/home" className='not-found-link'>Go to Home</a>
+    </div>
+  )
+}
+export default NotFound
